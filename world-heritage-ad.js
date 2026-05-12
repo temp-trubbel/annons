2bc60b5b54335f96b9d4cc0bd4d2f2de99c08ce0
azisam <azisam2k@icloud.com>
2026-05-12 11:26:26 +0200

    Geolocation API implementering för världsarv render

--- world-heritage-ad.js
+++ world-heritage-ad.js
@@ -58,7 +58,7 @@ class WorldHeritageAd extends HTMLElement {
         // Laddar in Shoelace
         this.loadShoelace();
         
-        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        // this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
 
     loadShoelace() {
@@ -78,6 +78,118 @@ class WorldHeritageAd extends HTMLElement {
             script.src = "https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.20.1/cdn/shoelace.js";
             script.dataset.shoelacejs = true;
             document.head.appendChild(script);
+        }
+    }
+
+    async connectedCallback() {
+        // Render skeleton
+        // code...
+
+        // Hitta användarens koordinater
+        const coordinates = await this.findMyCoordinates()
+        console.log("longitude:", coordinates.longitude);
+        console.log("latitude:", coordinates.latitude);
+
+        // GET världsarv från API
+        const worldHeritage = await this.getWorldHeritage(coordinates)
+        console.log(worldHeritage);
+
+        // Render final
+        this.render(worldHeritage)
+    }
+
+    render(worldHeritage) {
+        // Skapar HTML-mall
+        const template = document.createElement("template");
+        template.innerHTML =
+        `
+        <style>
+            .cardAd {
+                max-width: 600px;
+
+                & .cardImage {
+                    aspect-ratio: 4/1;
+                    object-fit: cover;
+                }
+
+                & .cardBody {
+                    display: flex;
+                    align-items: center;
+                    justify-content: space-between;
+
+                    sl-avatar {
+                        --size: 7rem;
+                    }
+
+                    & .cardText {
+                        h1, p {
+                            margin: 0;
+                        }
+                        
+                        & .shortDescription {
+
+                        }
+                    }
+                }
+            }
+        </style>
+
+        <sl-card class="cardAd">
+            <img class="cardImage" slot="image" src="${worldHeritage.main_image_url}" />
+
+            <div class="cardBody">
+                <sl-avatar shape="square">
+                    <sl-icon slot="icon" name="bank"></sl-icon>
+                </sl-avatar>
+
+                <div class="cardText">
+                    <h1>${worldHeritage.name_en}</h1>
+                    <p class="shortDescription">${worldHeritage.short_description_en}</p>
+                </div>
+
+                <sl-button variant="primary" size="large">Läs mer</sl-button>
+            </div>
+        </sl-card>
+        `
+
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+
+    async findMyCoordinates() {
+        if ("geolocation" in navigator) {
+            return new Promise((resolve, reject) => {
+                navigator.geolocation.getCurrentPosition(success, error);
+
+                function success(position) {
+                    const crd = position.coords;
+                    resolve(crd);
+                }
+
+                function error(error) {
+                    reject(error.message)
+                }
+            });
+        }
+        
+        else {
+            /* geolocation IS NOT available */
+            return {}
+        }
+    }
+
+    // bör ta in longitude & latittue från geolocation API
+    async getWorldHeritage(coords) {
+        try {
+            // Hämtar världsarv från Backend API
+            const response = await fetch(`http://localhost:8000/something?longitude=${coords.longitude}&latitude=${coords.latitude}`);
+            const data = await response.json();
+
+            // Retunerar kategorierna
+            return data;
+            
+        } catch (error) {
+            console.error(error);
+            return {};
         }
     }
 }
